ebe37de7136e66a3afc2aa0ec96f11af1a0bc7c4
drdhaval2785 <drdhaval2785@gmail.com>
2018-01-04 10:12:46 +0530

    more tests

--- tests/test_prakriya.py
+++ tests/test_prakriya.py
@@ -5,9 +5,28 @@
 
 
 import unittest
+import json
+import os.path
 from click.testing import CliRunner
 from prakriya import Prakriya
 from prakriya import cli
+
+
+def readJson(path):
+    """Read the given JSON file into python object."""
+    with open(path, 'r') as fin:
+        return json.load(fin)
+
+
+def compareresult(verbform, arguments=''):
+    """Compare the calculated and prestored results."""
+    p = Prakriya()
+    calculated = p[verbform, arguments]
+    wholedata = readJson(os.path.join('tests', 'testdata', verbform + '.json'))
+    if arguments == '':
+        assert(calculated == wholedata)
+    else:
+        assert(calculated == wholedata[arguments])
 
 
 class TestPrakriya(unittest.TestCase):
@@ -15,15 +34,33 @@ class TestPrakriya(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-        self.bhavati = [[{'sutra': u'BUvAdayo DAtavaH', 'sutra_num': u'1.3.1', 'form': u'BU'}, {'sutra': u'laH karmaRi ca BAve cAkarmakeByaH.', 'sutra_num': u'3.4.69', 'form': u'BU'}, {'sutra': u'vartamAne law', 'sutra_num': u'3.2.123', 'form': u'BU+la~w'}, {'sutra': u'lasya', 'sutra_num': u'3.4.77', 'form': u'BU+la~w'}, {'sutra': u'halantyam', 'sutra_num': u'1.3.3', 'form': u'BU+la~w'}, {'sutra': u'tasya lopaH', 'sutra_num': u'1.3.9', 'form': u'BU+la~'}, {'sutra': u"upadeSe'janunAsika it", 'sutra_num': u'1.3.2', 'form': u'BU+la~'}, {'sutra': u'tasya lopaH', 'sutra_num': u'1.3.9', 'form': u'BU+l'}, {'sutra': u'tiptasJisipTasTamibvasmas tAtAMJaTAsATAMDvamiqvahimahiN', 'sutra_num': u'3.4.78', 'form': u'BU+tip'}, {'sutra': u'laH parasmEpadam', 'sutra_num': u'1.4.99', 'form': u'BU+tip'}, {'sutra': u'tiNastrIRi trIRi praTamamaDyamottamAH', 'sutra_num': u'1.4.101', 'form': u'BU+tip'}, {'sutra': u'tAnyekavacanadvivacanabahuvacanAnyekaSaH', 'sutra_num': u'1.4.102', 'form': u'BU+tip'}, {'sutra': u'Seze praTamaH', 'sutra_num': u'1.4.108', 'form': u'BU+tip'}, {'sutra': u'tiNSitsArvaDAtukam', 'sutra_num': u'3.4.113', 'form': u'BU+tip'}, {'sutra': u'kartari Sap\u200c', 'sutra_num': u'3.1.68', 'form': u'BU+Sap+tip'}, {'sutra': u'tiNSitsArvaDAtukam', 'sutra_num': u'3.4.113', 'form': u'BU+Sap+tip'}, {'sutra': u'laSakvatadDite', 'sutra_num': u'1.3.8', 'form': u'BU+Sap+tip'}, {'sutra': u'tasya lopaH', 'sutra_num': u'1.3.9', 'form': u'BU+ap+tip'}, {'sutra': u'halantyam', 'sutra_num': u'1.3.3', 'form': u'BU+ap+tip'}, {'sutra': u'tasya lopaH', 'sutra_num': u'1.3.9', 'form': u'BU+a+ti'}, {'sutra': u'sArvaDAtukArDaDAtukayoH', 'sutra_num': u'7.3.84', 'form': u'Bo+a+ti'}, {'sutra': u"eco'yavAyAvaH", 'sutra_num': u'6.1.78', 'form': u'Bav+a+ti'}, {'sutra': u'antimaM rUpam', 'sutra_num': u'-2', 'form': u'Bavati'}]]
+
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_000_something(self):
         """Test something."""
-        p = Prakriya()
-        output = p['Bavati', 'prakriya']
-        assert(output == self.bhavati)
+        compareresult('Bavati')
+        compareresult('Bavati', 'prakriya')
+        compareresult('Bavati', 'verb')
+        compareresult('Bavati', 'verbaccent')
+        compareresult('Bavati', 'lakara')
+        compareresult('Bavati', 'gana')
+        compareresult('Bavati', 'meaning')
+        compareresult('Bavati', 'number')
+        compareresult('Bavati', 'madhaviya')
+        compareresult('Bavati', 'kshiratarangini')
+        compareresult('Bavati', 'dhatupradipa')
+        compareresult('Bavati', 'jnu')
+        compareresult('Bavati', 'uohyd')
+        compareresult('Bavati', 'upasarga')
+        compareresult('Bavati', 'padadecider_id')
+        compareresult('Bavati', 'padadecider_sutra')
+        compareresult('Bavati', 'it_id')
+        compareresult('Bavati', 'it_status')
+        compareresult('Bavati', 'it_sutra')
+        compareresult('Bavati', 'purusha')
+        compareresult('Bavati', 'vachana')
 
     def test_command_line_interface(self):
         """Test the CLI."""

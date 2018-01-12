ab1eb44abd3943eddd4ab578fefd9afc2b08eb88
drdhaval2785 <drdhaval2785@gmail.com>
2018-01-12 21:39:47 +0530

    class improved

--- prakriya/__init__.py
+++ prakriya/__init__.py
@@ -5,8 +5,8 @@
 __author__ = """Dr. Dhaval Patel"""
 __email__ = 'drdhaval2785@gmail.com'
 __version__ = '0.0.4'
-__all__ = ['Prakriya', 'getform']
+__all__ = ['Prakriya', 'Generate']
 
 
 from .verbforms import Prakriya
-from .generate import getform
+from .generate import Generate

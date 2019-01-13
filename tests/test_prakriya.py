60f210ffbcee7c87c2a7a64c0e9141c192328afb
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2019-01-13 18:44:37 +0530

    Comparetranslit uses method now

--- tests/test_prakriya.py
+++ tests/test_prakriya.py
@@ -22,7 +22,7 @@ def comparetranslit(verbform, inTran, outTran, arguments=''):
     p = Prakriya()
     p.inputTranslit(inTran)
     p.outputTranslit(outTran)
-    calculated = p[verbform, arguments]
+    calculated = p.get_info(verbform, arguments)
     superdata = readJson(os.path.join('tests', 'testdata', 'Bavati.json'))
     wholedata = superdata[outTran]
     if arguments == '':
